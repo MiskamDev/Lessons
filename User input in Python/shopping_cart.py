59a6429aa62c56ce7a2dafc4bb32d3fcb4a8ed99
MiskamDev <pretulencomaxim533@gmail.com>
2025-08-01 00:23:37 +0300

    Typos have been fixed and shopping cart withdrawal messages have been improved.

--- User input in Python/shopping_cart.py
+++ User input in Python/shopping_cart.py
@@ -1,8 +1,9 @@
-item = input("What item woule you like to buy?: ")
-price = float(input("What is the price?: "))
-quantity = int(input("How many would you like?: "))
+item = input("What item would you like to buy?: ")  # User input for item
+price = float(input("What is the price?: "))        # User input for price
+quantity = int(input("How many would you like?: ")) # User input for quantity
 
-total = price * quantity
+total = price * quantity  # Calculating total cost
 
-print(f"You have bougth {quantity} x {item}/s")
-print(f"You total is: {round(total, 2)}$")
+print(f"You have bought {quantity} x {item}/s") # Displaying the item and quantity
+print(f"Price per item: {price}$")              # Displaying the price per item
+print(f"Your total is: {round(total, 2)}$")     # Displaying the total cost
